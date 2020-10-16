628a0f05c906bae9fb4c0c919ba606d1255877e9
SaulGh <72111261+SaulGh@users.noreply.github.com>
2020-10-16 15:28:53 -0500

    Add files via upload

--- matxmat_u&j.c
+++ matxmat_u&j.c
@@ -4,34 +4,29 @@
 
 int main(){
     FILE *fptr0;
-    fptr0=fopen("matrix_n.txt","w");
+    fptr0=fopen("matrix_u&j.txt","w");
     int n = 40;
     int matrix[n-1][n-1];
+    float matrix2[n-1][n-1];
     int i,j,k;
     float t1, t2, tf;
-    float matrix2[n-1][n-1];
     t1 = omp_get_wtime();
     
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
+    for (i = 0; i < n; i++){
+        for (j = 0; j < n; j++){
             matrix[i][j]=1.0;
         }
-        
     }
     
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
+    for (i = 0; i < n; i++){
+        for (j = 0; j < n; j+=2){
             matrix2[i][j]=0;
-            for (k = 0; k < n; k++)
-            {
-                matrix2[i][j]+=(matrix[i][k]*matrix[k][j]);
+            matrix2[i][j+1]=0;
+            for (k = 0; k < n; k++){
+                matrix2[i][j]=matrix[i][k]*matrix[k][j]+matrix2[i][j];
+                matrix2[i][j+1]=matrix[i][k]*matrix[k][j+1]+matrix2[i][j+1];
             }
         }
-        
     }
 
     for (i = 0; i < n; i++)
